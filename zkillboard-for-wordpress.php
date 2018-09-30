e22c9eac87d15249bc61b0dbb853c77626797fa8
Peter Pfeufer <ppfeufer@gmail.com>
2018-10-01 00:19:31 +0200

    First files. Does not much yet, but we have to start somewhere ...

---
+++ zkillboard-for-wordpress.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Plugin Name: Zkillboard for WordPress
+ * Plugin URI: https://github.com/ppfeufer/zkillboard-for-wordpress
+ * Git URI: https://github.com/ppfeufer/zkillboard-for-wordpress
+ * Description: zKillboard implementation for WordPress
+ * Version: 0.0.1
+ * Author: Rounon Dax
+ * Author URI: https://terra-nanotech.de
+ * Text Domain: zkillboard-for-wordpress
+ * Domain Path: /l10n
+ */
+
+/*
+ * Copyright (C) 2018 Rounon Dax
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace WordPress\Plugins\ZkillboardForWordPress;
+
+const WP_GITHUB_FORCE_UPDATE = true;
+
+\defined('ABSPATH') or die();
+
+class ZkillboardForWordPress {
+
+}
